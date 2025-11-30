9676f1c655d032c540c72a7d366a53e0741b6900
Tainara <tainarasantossilva980@gmail.com>
2025-11-30 17:29:32 -0300

    fix: ajuste responsividade

--- frontend/src/app/components/header/header.component.ts
+++ frontend/src/app/components/header/header.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 import { AuthService } from '../../services/auth/auth.service';
 import { NgIf } from '@angular/common';
 import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-header',
-  imports: [NgIf, RouterLink],
+  imports: [RouterOutlet, NgIf, RouterLink],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
