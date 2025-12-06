0f9bdae13d289d63ca89ab171db6899dc404d9bd
Tainara <tainarasantossilva980@gmail.com>
2025-12-06 17:58:07 -0300

    fix: correcao cadastro usuarios

--- frontend/src/app/services/auth/auth.interceptor.spec.ts
+++ frontend/src/app/services/auth/auth.interceptor.spec.ts
@@ -1,16 +1,19 @@
 import { TestBed } from '@angular/core/testing';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { AuthInterceptor } from './auth.interceptor';
+import { AuthService } from './auth.service';
 
-import { AuthInterceptor } from './auth.interceptor.js';
-
-describe('AuthInterceptor', () => {
-  let service: AuthInterceptor;
-
-  beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(AuthInterceptor);
-  });
-
-  it('should be created', () => {
-    expect(service).toBeTruthy();
+beforeEach(() => {
+  TestBed.configureTestingModule({
+    imports: [HttpClientTestingModule],
+    providers: [
+      AuthService,
+      {
+        provide: HTTP_INTERCEPTORS,
+        useClass: AuthInterceptor,
+        multi: true
+      }
+    ]
   });
 });
